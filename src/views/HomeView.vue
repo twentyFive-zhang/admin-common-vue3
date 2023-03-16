e81434d9878166103bcf476123ee79d5c469fb31
二五 <1097983662@qq.com>
2023-03-17 01:05:15 +0800

    fix: code format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
-import { Button } from 'ant-design-vue';
+import { Button } from 'ant-design-vue'
 </script>
 
 <template>
